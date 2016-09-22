9cc1cd62386e11fff788712ca1c3acf39502412e
pedro.moreira <pedro.moreira@positiveblue.pt>
2016-09-22 16:12:59 +0100

    Initial commit

---
+++ DbUpdate/Program.cs
@@ -0,0 +1,54 @@
+﻿using DbUp;
+using System;
+
+namespace DbUpdate
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Usage: DbUpdate <ConnectionString> <MigrationsPath>");
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+
+                return -1;
+            }
+
+            string connectionString = args[0];
+            string migrationsPath = args[1];
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsFromFileSystem(migrationsPath)
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+
+            return 0;
+        }
+    }
+}
